55725d6c68b5d83883e57485f7a18d117dd800cb
david <davidsinternetmail@gmail.com>
2022-08-09 16:43:56 -0700

    added user choice for grid size

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 let container = document.getElementById("grid");
 
-function makeGrid() {
+let button = document.getElementsByClassName("adjust");
+button[0].onclick = function() {
+    console.log("clicked");
+    let size = window.prompt("\t\t\t\tWhat size would you like the grid?\n\t\t\t\t     (must be between 1 and 100)");
+    destroyGrid();
+    makeGrid(size);
+}
+
+function makeGrid(size) {
     let id = 0;
-    for (let row = 0; row < 4; row++) {
+    for (let row = 0; row < size; row++) {
         let rows = document.createElement("div");
         rows.setAttribute("class", "row");
         rows.setAttribute("id", row)
         container.append(rows);
-        for (let col = 0; col < 4; col++) {
+        for (let col = 0; col < size; col++) {
             let cell = document.createElement("div");
             cell.setAttribute("id", id++);
             cell.setAttribute("class", "cell");
-            cell.innerHTML = "CELL";
             cell.style.display = "inline";
             rows.appendChild(cell);
         }
     }
 }
 
-makeGrid();
+function destroyGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.children[0]);
+    }
+}
+
+makeGrid(4);
